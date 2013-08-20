f05389a77ec1f4f17b37f7c2f776714a8241006d
Alder <farvistadw@gmail.com>
2013-08-20 09:17:02 +0400

    10

--- 10.rb
+++ 10.rb
@@ -1,7 +1,17 @@
-require "prime"
-r=0
-Prime.each(10000000) do |p|
-  break if p > 2000000
-  r+=p
-end
-p r
+# require "prime"
+# r=0
+# Prime.each(10000000) do |p|
+#   break if p > 2000000
+#   r+=p
+# end
+# p r
+
+
+require 'mathn'
+puts "Started at #{Time.now}."
+sum = Prime.new.inject(0) { |sum, n|
+  break sum unless n < 1_000_000
+  sum + n
+}
+puts "Finished at #{Time.now}."
+puts "Sum is #{sum}."
