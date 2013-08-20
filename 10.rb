18e547cae92b1c7286424754d314b7c0bd51d9e2
Alder <farvistadw@gmail.com>
2013-08-20 09:21:06 +0400

    10

--- 10.rb
+++ 10.rb
@@ -8,10 +8,10 @@
 
 
 require 'mathn'
-puts "Started at #{Time.now}."
-sum = Prime.new.inject(0) { |sum, n|
-  break sum unless n < 1_000_000
+started = Time.now
+sum = Prime.instance.inject(0) { |sum, n|
+  break sum unless n < 2_000_000
   sum + n
 }
-puts "Finished at #{Time.now}."
+puts "Finished at #{(Time.now - started)}."
 puts "Sum is #{sum}."
