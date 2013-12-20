76b7fcbf70ac524b619fdcc9c0ed857cf162f1de
Alder <farvistadw@gmail.com>
2013-12-21 00:17:31 +0400

    small fix

--- euler_project/050.rb
+++ euler_project/050.rb
@@ -5,8 +5,7 @@ i = 0
 
 Prime.each(1_000_000) do |x|
   a[i] = 0 if i < 100
-  a.each { |k, v| a[k] += x if (a[k] + x) < 1_000_000 }
-  break if a[0] > 1_000_000
+  a.each { |k, _| a[k] += x if (a[k] + x) < 1_000_000 }
   i += 1
 end
 
